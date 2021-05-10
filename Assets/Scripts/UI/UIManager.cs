4aee300015241844699de4f96f55ad752b73cdc5
chrisdfe <christopher.d.ferris@gmail.com>
2021-05-09 22:59:04 -0700

    Renamed GUI to UI

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -6,7 +6,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class GUIManager : MonoBehaviour
+namespace TowerBuilder.UI
 {
+    public class UIManager : MonoBehaviour
+    {
 
+    }
 }
