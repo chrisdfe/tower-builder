43aa00d56dceb268007a01d7b90dc097b68d846c
Christopher Ferris <christopher.d.ferris@gmail.com>
2022-03-30 18:19:22 -0700

    Deleted rooms/_modules

--- Assets/Scripts/Stores/Rooms/_modules/RoomModuleDetailsBase.cs
+++
@@ -1,7 +0,0 @@
-// namespace TowerBuilder.Stores.Rooms.Modules
-// {
-//     public abstract class RoomModuleDetailsBase
-//     {
-//         public abstract RoomModuleKey roomModuleKey { get; }
-//     }
-// }
