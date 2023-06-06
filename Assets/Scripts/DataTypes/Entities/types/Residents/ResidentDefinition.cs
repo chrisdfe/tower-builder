ac5330bc8bc5610f2a4497bb54cae2375371a652
Christopher Ferris <christopher.d.ferris@gmail.com>
2023-06-06 11:27:53 -0700

    Fixed resident resizability

--- Assets/Scripts/DataTypes/Entities/types/Residents/ResidentDefinition.cs
+++ Assets/Scripts/DataTypes/Entities/types/Residents/ResidentDefinition.cs
@@ -2,6 +2,7 @@ namespace TowerBuilder.DataTypes.Entities.Residents
 {
     public class ResidentDefinition : EntityDefinition<Resident.Key>
     {
+        public override Resizability resizability => Resizability.Inflexible;
         public override ValidatorFactory validatorFactory => (Entity entity) => new ResidentValidator(entity as Resident);
     }
 }
