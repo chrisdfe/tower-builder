f8a8bbd47383c13d3eaed59e10fd209cc10f949f
Christopher Ferris <christopher.d.ferris@gmail.com>
2023-06-12 16:02:01 -0700

    Forgot to save BuildingDefinition

---
+++ Assets/Scripts/DataTypes/EntityGroups/Buildings/BuildingDefinition.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using System.Linq;
+using TowerBuilder.ApplicationState;
+using TowerBuilder.DataTypes.Behaviors.Furnitures;
+using TowerBuilder.DataTypes.Entities;
+using TowerBuilder.DataTypes.Entities.Furnitures;
+using TowerBuilder.DataTypes.EntityGroups.Rooms;
+using UnityEngine;
+
+namespace TowerBuilder.DataTypes.EntityGroups.Buildings
+{
+    public class BuildingDefinition : EntityGroupDefinition
+    {
+        public BuildingDefinition() { }
+    }
+}
